4a12c271bca8fa7510f008c794834fa51f323132
Robin Burri <robinburri@tutanota.com>
2022-10-30 14:17:33 +0100

    changed entrypoint.sh mariadb

--- srcs/requirements/mariadb/scripts/entrypoint.sh
+++ srcs/requirements/mariadb/scripts/entrypoint.sh
@@ -1,19 +1,17 @@
-cat .setup 2> /dev/null
+cat .done 2> /dev/null
 if [ $? -ne 0 ]; then
-	# https://dev.mysql.com/doc/refman/8.0/en/mysqld-safe.html
-	# Le & va etre utilise pour effectuer une "modification sur le serveur MySQL" - mysql driver avec des options
 	usr/bin/mysqld_safe --datadir=/var/lib/mysql &
 
-	# Il est necessaire d'attendre que la base de donnee soit bien accessible, mysql lance
 	while ! mysqladmin ping -h "$MARIADB_HOST" --silent; do
     	sleep 1
 	done
 
 	eval "echo \"$(cat /tmp/create_db.sql)\"" | mariadb
-	touch .setup
+	touch .done
+else
+	usr/bin/mysqld_safe --datadir=/var/lib/mysql
 fi
-# Lancement du serveur mysql de facon la plus securisee sur Uinix
-usr/bin/mysqld_safe --datadir=/var/lib/mysql
+
 
 # TO Check users in the database:
 
